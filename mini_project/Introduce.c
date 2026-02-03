5113a37c9453a9f84e1aaad22e09c780512373b8
Nguyễn Thông <thongnguyendev75@gmail.com>
2026-02-03 11:59:50 +0700

    Add files via upload

---
+++ mini_project/Introduce.c
@@ -0,0 +1,13 @@
+/*Introduce myself*/
+#include "stdio.h"
+int main(){
+          int Age = 19;
+          char a;
+          float kg = 50.5;
+          printf("\nEnter the first character name: ");
+          scanf(" %c", &a);
+
+          printf("Hello everyone my name Toidicodelai brother Toidicodedao: %c", a);
+          printf("\nI'm %d years old", Age);
+          printf("\nI'm weight %.1f kg", kg);
+}
