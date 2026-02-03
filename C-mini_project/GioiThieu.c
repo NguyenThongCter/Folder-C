512eba9936c343c02c82236ace0155e2db45aac3
Nguyễn Thông <thongnguyendev75@gmail.com>
2026-02-03 11:39:46 +0700

    IntroduceMyself

---
+++ C-mini_project/GioiThieu.c
@@ -0,0 +1,13 @@
+/*Introduce myself*/
+#include "stdio.h"
+int main(){
+          int tuoi = 19;
+          char a;
+          float kg = 50.5;
+          printf("\nNhap vao ky tu dau ten cua minh: ");
+          scanf(" %c", &a);
+
+          printf("Hello cac ban minh la toidicodelai em cua toidicodedao minh ten: %c", a);
+          printf("\nNam nay minh %d tuoi", tuoi);
+          printf("\nMinh nang %.1f kg", kg);
+}
